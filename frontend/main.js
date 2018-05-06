6e89b662200f2f592708054153d1d863f16f6772
maxuekai <Kkkat>
2018-05-06 13:37:21 +0800

    :construction: add gruntfile to release app

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-const { ipcMain, session } = require('electron');
+const { ipcMain } = require('electron');
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
@@ -7,59 +7,59 @@ const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
+const cp = require('child_process');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, loading;
 
-function createWindow() {
+function createWindow () {
+  loading = new BrowserWindow({ show: false, frame: false });
 
-    loading = new BrowserWindow({ show: false, frame: false });
-
-    loading.once('show', () => {
+  loading.once('show', () => {
         // Create the browser window.
-        mainWindow = new BrowserWindow({ width: 1014, height: 680, show: false, frame: false, title: 'Hakon' });
-        mainWindow.webContents.once('dom-ready', () => {
-            console.log('main loaded');
-            mainWindow.show();
-            loading.hide();
-            loading.close();
-        });
-        // and load the index.html of the app.
-        // mainWindow.loadURL(url.format({
-        //   pathname: path.join(__dirname, '/dist/index.html'),
-        //   protocol: 'file:',
-        //   slashes: true
-        // }));
-        mainWindow.loadURL('http://127.0.0.1:8080/#/moduleManageTool/', {});
+    mainWindow = new BrowserWindow({ width: 1014, height: 680, show: false, frame: false, title: 'Hakon' });
+    mainWindow.webContents.once('dom-ready', () => {
+      console.log('main loaded');
+      mainWindow.show();
+      loading.hide();
+      loading.close();
     });
-    loading.loadURL(`file://${__dirname}/dist/loading.html`);
-    loading.show();
+        // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, '/dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+    // mainWindow.loadURL('http://127.0.0.1:8080/#/moduleManageTool/', {});
+  });
+  loading.loadURL(`file://${__dirname}/dist/loading.html`);
+  loading.show();
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null;
-    });
-    ipcMain.on('min', e => mainWindow.minimize());
-    ipcMain.on('max', e => {
-        if (mainWindow.isMaximized()) {
-            mainWindow.unmaximize();
-        } else {
-            mainWindow.maximize();
-        }
-    });
-    ipcMain.on('close', e => mainWindow.close());
-    ipcMain.on('webview', e => {
-        let webview = new BrowserWindow({ width: 1014, height: 680, show: true, frame: true, autoHideMenuBar:true });
-        webview.loadURL('http://127.0.0.1:8080/#/admin/login', {});
-        webview.webContents.openDevTools();
-    });
+    mainWindow = null;
+  });
+  ipcMain.on('min', e => mainWindow.minimize());
+  ipcMain.on('max', e => {
+    if (mainWindow.isMaximized()) {
+      mainWindow.unmaximize();
+    } else {
+      mainWindow.maximize();
+    }
+  });
+  ipcMain.on('close', e => mainWindow.close());
+  ipcMain.on('webview', e => {
+    let webview = new BrowserWindow({ width: 1014, height: 680, show: true, frame: true, autoHideMenuBar: true });
+    webview.loadURL('http://127.0.0.1:8080/#/admin/login', {});
+    webview.webContents.openDevTools();
+  });
 }
 
 // This method will be called when Electron has finished
@@ -68,21 +68,61 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
+// squirrel
+function handleSquirrelEvent () {
+  if (process.platform !== 'win32') {
+    return false;
+  }
+  function executeSquirrelCommand (args, done) {
+    let updateDotExe = path.resolve(path.dirname(process.execPath), '..', 'update.exe');
+    let child = cp.spawn(updateDotExe, args, { detached: true });
+    child.on('close', function () {
+      done();
+    });
+  }
+  function install (done) {
+    let target = path.basename(process.execPath);
+    executeSquirrelCommand(['--createShortcut', target], done);
+  }
+  function uninstall (done) {
+    let target = path.basename(process.execPath);
+    executeSquirrelCommand(['--removeShortcut', target], done);
+  }
+  let squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+      alert('hello');
+      install(app.quit);
+      return true;
+    case '--squirrel-updated':
+      install(app.quit);
+      return true;
+    case '--squirrel-obsolete':
+      app.quit();
+      return true;
+    case '--squirrel-uninstall':
+      uninstall(app.quit);
+      return true;
+  }
+  return false;
+}
+handleSquirrelEvent();
+// end squirrel
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
